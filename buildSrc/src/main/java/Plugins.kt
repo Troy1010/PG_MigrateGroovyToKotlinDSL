d29048a02e0c91f5c1097371b10fa4f6a3ab9adc
Troy <troyswanson1010@gmail.com>
2020-10-11 01:24:11 -0500

    using buildSrc objects

---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,8 @@
+object Plugins {
+    const val androidApplication = "com.android.application"
+    object Kotlin {
+        const val kapt = "kapt"
+        const val android = "android"
+        const val androidExtensions = "android.extensions"
+    }
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,5 @@
+object Versions {
+    const val kotlin = "1.3.10"
+    const val ktx = "1.0.0"
+    const val junit = "4.12"
+}
