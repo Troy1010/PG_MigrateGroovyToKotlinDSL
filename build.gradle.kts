94bfc4cf14f60dab7b1b8086b7e9d3a759f03867
Troy <troyswanson1010@gmail.com>
2020-10-11 01:10:37 -0500

    moved buildscript block to buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        jcenter()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:4.0.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
-    }
-}
-
 allprojects {
     repositories {
         google()
